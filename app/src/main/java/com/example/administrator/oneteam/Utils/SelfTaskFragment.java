efcc32536114c1da040c7a8ccc9cea949b4bbdf1
vincent <2315223212@qq.com>
2017-12-31 13:57:58 +0800

    Add GreetingText 并在SelfTaskFragment中使用子线程调用

--- app/src/main/java/com/example/administrator/oneteam/Utils/SelfTaskFragment.java
+++ app/src/main/java/com/example/administrator/oneteam/Utils/SelfTaskFragment.java
@@ -1,6 +1,9 @@
 package com.example.administrator.oneteam.Utils;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.administrator.oneteam.R;
+import com.example.administrator.oneteam.tools.GreetingText;
 
 /**
  * Created by D105-01 on 2017/12/24.
@@ -15,6 +19,32 @@ import com.example.administrator.oneteam.R;
 
 public class SelfTaskFragment extends Fragment {
 
+    private static final int UPDATE_GREETING_TEXT = 1;
+
+    private GreetingText gt_greetingText;
+
+
+    @SuppressLint("HandlerLeak")
+    //region 线程处理函数 Handler
+    final Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what){
+                case UPDATE_GREETING_TEXT:
+                    gt_greetingText.update();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+    //endregion
+
+    /**
+     * 用于在main中实例化SelfTaskFragment
+     * @return
+     */
     public static SelfTaskFragment newInstance(){
         return new SelfTaskFragment();
     }
@@ -22,7 +52,12 @@ public class SelfTaskFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.self_task_fragment, null);
+        View view = inflater.inflate(R.layout.self_task_fragment, null);
+
+        gt_greetingText = (GreetingText) view.findViewById(R.id.gt_self_task_fragment);
+        setUpGreetingText();
+
+        return view;
     }
 
     @Override
@@ -33,5 +68,24 @@ public class SelfTaskFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+    }
+
+    
+    /**
+     * 子线程更新GreetingText
+     * 回调函数是Handler
+     */
+    private void setUpGreetingText(){
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Message msg = new Message();
+                msg.what = UPDATE_GREETING_TEXT;
+                handler.sendMessage(msg);
+                //每隔十分钟执行一次GreetingText的更新
+                handler.postDelayed(this, 1000*60*10);
+            }
+        };
+        runnable.run();
     }
 }
