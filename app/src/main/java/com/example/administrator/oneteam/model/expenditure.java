eebc0c04e9f97d482150a2983d4100736272041c
[zpw] <1048392480@qq.com>
2018-01-01 14:11:42 +0800

    update model class

---
+++ app/src/main/java/com/example/administrator/oneteam/model/expenditure.java
@@ -0,0 +1,17 @@
+package com.example.administrator.oneteam.model;
+
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2018/1/1 0001.
+ */
+
+public class expenditure {
+    public int expenditure_id;
+    public int person_id;
+    public int task_id;
+    public String expenditure_description;
+    public String expenditure_date;
+    public String state;
+    public int money;
+}
--- app/src/main/java/com/example/administrator/oneteam/model/time_sheet.java
+++ app/src/main/java/com/example/administrator/oneteam/model/time_sheet.java
@@ -5,8 +5,8 @@ package com.example.administrator.oneteam.model;
  */
 
 public class time_sheet {
-    public String time_sheet_id;
-    public String person_id;
+    public int time_sheet_id;
+    public int person_id;
     public String date;
     public String start_time;
     public String total_time;
