6a1ea572b89c144fac4d932b37c4124ffe9e5941
vincent <2315223212@qq.com>
2018-01-19 15:44:30 +0800

    修改报销界面，使用人工干预技术生成一条未报销记录

--- app/src/main/java/com/example/administrator/oneteam/model/Expenditure.java
+++ app/src/main/java/com/example/administrator/oneteam/model/Expenditure.java
@@ -7,6 +7,7 @@ import java.util.Date;
  */
 
 public class Expenditure {
+
     public int expenditure_id;
     public int person_id;
     public int task_id;
@@ -14,4 +15,24 @@ public class Expenditure {
     public String expenditure_date;
     public String state;
     public int money;
+
+    public Expenditure(int expenditure_id, int person_id, int task_id, String expenditure_description, String expenditure_date, String state, int money) {
+        this.expenditure_id = expenditure_id;
+        this.person_id = person_id;
+        this.task_id = task_id;
+        this.expenditure_description = expenditure_description;
+        this.expenditure_date = expenditure_date;
+        this.state = state;
+        this.money = money;
+    }
+
+    public Expenditure() {
+        this.expenditure_id = 1;
+        this.person_id = 1;
+        this.task_id = 1;
+        this.expenditure_description = "desp";
+        this.expenditure_date = "date";
+        this.state = "state";
+        this.money = 1;
+    }
 }
